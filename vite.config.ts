fbd66a947ca99d7ef71c2ef847413df813883678
Peixeweb <140914200+peixeweb@users.noreply.github.com>
2026-09-21 15:27:52 -0300

    feat: deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Deus/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(import.meta.dirname, '.'),
       },
     },
     server: {
